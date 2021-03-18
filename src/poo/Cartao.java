95d597269afb8ab84ca0f0cbb987894b4f6c5dae
David <Dracarreira@gmail.com>
2021-03-18 18:28:29 +0000

    Javadoc Adicionado

--- src/poo/Cartao.java
+++ src/poo/Cartao.java
@@ -1,17 +1,33 @@
 package poo;
-
+/**
+ * 
+ * 
+ * @author Daviw
+ *
+ *classe Cartão serve para criar o objeto cartão
+ */
 public class Cartao {
-	
 	private String nomeFuncionario;
 	private String servico;
 	private String cargo;
 	
 	// Construtores 
+	/**
+	 * Um cartão só com nome de funcionario
+	 * 
+	 * @param nomeFuncionario String do nome do funcionario
+	 */
 	public Cartao(String nomeFuncionario) {
 		super();
 		this.nomeFuncionario = nomeFuncionario;
 	}
-
+    /**
+     * Um cartão com 3 campos (nome de funcionario,serviço e cargo)
+     * 
+     * @param nomeFuncionario String do nome do funcionario
+     * @param servico String do tipo de serviço
+     * @param cargo String do tipo de cargo
+     */
 	public Cartao(String nomeFuncionario, String servico, String cargo) {
 		super();
 		this.nomeFuncionario = nomeFuncionario;
@@ -20,35 +36,62 @@ public class Cartao {
 	}
 
 	// Acessores
-	
+	/**
+	 * Obter o nome de funcionario
+	 * 
+	 *  @return Retorna a string com o nome do funcionario
+	 */
 	public String getNomeFuncionario() {
 		return nomeFuncionario;
 	}
-
+     /**
+      * Permite alterar os serviços
+      * 
+      * @param servico Modifica o serviço 
+      */
 	public void setServico(String servico) {
 		this.servico = servico;
 	}
-
+     /**
+      * Permite alterar o cargo
+      *  
+      * @param cargo Modifica o cargo
+      * 
+      */
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
 	}
 	
 	// Métodos
+	 /**
+	  * Põe o serviço como vazio
+	  */
 	public void retirarServico() {
 		// servico = "";
 		setServico("");
 	}
-	
+	/**
+	 * Permite criar novos serviços
+	 * 
+	 * @param novoServico Cria novos serviços
+	 */
 	public void atribuirNovoServico( String novoServico ) {
 		//servico = novoServico;
 		setServico( novoServico );
 	}
-	
+	/**
+	 * Promove novo cargo
+	 * 
+	 * @param novoCargo o cargo que se pretende
+	 */
 	public void promoverNovoCargo( String novoCargo ) {
 		//cargo = novoCargo;
 		setCargo(novoCargo);
 	}
-	
+	/**
+	 * Despedir o funcionário
+	 * 
+	 */
 	public void despedirFuncionario() {
 		retirarServico();
 		setCargo("");
